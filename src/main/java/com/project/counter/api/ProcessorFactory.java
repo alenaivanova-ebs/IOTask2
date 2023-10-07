20ad580400537519289a351b50a0102e387f1b16
alenaivanova <63234472+alenaivanova-ebs@users.noreply.github.com>
2023-10-08 00:00:20 +0300

    Initial Commit

---
+++ src/main/java/com/project/counter/api/ProcessorFactory.java
@@ -0,0 +1,18 @@
+package com.project.counter.api;
+
+import java.io.FileNotFoundException;
+
+/**
+ * An interface for the construction of {@link FileProcessor}s which limit the amount of data
+ * they process.
+ */
+public interface ProcessorFactory {
+    /**
+     * Returns a new {@link FileProcessor} which limits input processing as configured.
+     *
+     * @param fileName the file name to be processed.
+     * @return a {@link FileProcessor} which reads orders from its input stream and writes the
+     * result of processing said orders out its output stream.
+     */
+    FileProcessor createProcessor(String fileName) throws FileNotFoundException;
+}
